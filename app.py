9ffbc9a32f841b0bc936d3482edbd2c4eead7e54
504423009 <74481993+504423009@users.noreply.github.com>
2026-05-12 23:20:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def upload_file():
         return jsonify({"filename": unique_filename}) # 只返回文件名即可
     return jsonify({"error": "File type not allowed"}), 400
 
-@app.route('/api/generate', methods=['POST'])
+@app.route('/generate', methods=['POST'])
 def generate():
     data = request.json
     uploaded_filename = data.get('filename')
